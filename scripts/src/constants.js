5fb8b6fc5bbff426f5e8f8add932656bbc28d7d3
Unknown <apolzien@oyster.com>
2017-10-09 19:09:47 -0400

    script to generate a mock DB from the sounds folder

---
+++ scripts/src/constants.js
@@ -0,0 +1,5 @@
+import process from 'process';
+import path from 'path';
+
+export const SOUNDS_DIR = path.join(process.cwd(), 'sounds');
+export const MOCK_DATA_DIR = path.join(process.cwd(), 'mock_data');
--- scripts/src/temp-db.js
+++ scripts/src/temp-db.js
@@ -1,1 +1,47 @@
-console.log('hello temp-db.js!');
+import AV from 'av';
+import fs from 'fs';
+import path from 'path';
+
+import {MOCK_DATA_DIR, SOUNDS_DIR} from './constants';
+
+const dirBuffer = fs.readdirSync(SOUNDS_DIR);
+const fileNames = [];
+for (const fileName of dirBuffer) {
+  fileNames.push(fileName);
+}
+
+const isWav = fileName => fileName.endsWith('.wav');
+const promises = fileNames.filter(isWav).map(fileName =>
+  new Promise(
+    (resolve, reject) => {
+      const absPath = path.join(SOUNDS_DIR, fileName);
+      fs.readFile(absPath,
+        (err, fileBuffer) => {
+          const asset = AV.Asset.fromBuffer(fileBuffer);
+          asset.decodeToBuffer(data => {
+            resolve({
+              fileName,
+              sampleRate: asset.format.sampleRate,
+              path: absPath
+            });
+          });
+        }
+      );
+    }
+  )
+);
+
+Promise.all(promises).then(
+  fileInfos => {
+    const jsonData = JSON.stringify(fileInfos);
+    const dataFilePath = path.join(MOCK_DATA_DIR, 'mock-db.json');
+    fs.writeFile(dataFilePath, jsonData,
+      err => {
+        if (err) {
+          throw(err);
+        }
+        console.log('wrote data for ${fileInfos.length} files');
+      }
+    );
+  }
+);
