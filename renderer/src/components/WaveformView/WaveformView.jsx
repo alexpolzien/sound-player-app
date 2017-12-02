3fb06a2ef36ef208ceef3e41ccecbeb29831dfd3
Unknown <apolzien@oyster.com>
2017-12-02 16:08:24 -0500

    resizable canvas

--- renderer/src/components/WaveformView/WaveformView.jsx
+++ renderer/src/components/WaveformView/WaveformView.jsx
@@ -2,12 +2,52 @@ import React from 'react';
 
 import styles from './WaveformView.css';
 
+class WaveformCanvas extends React.Component {
+  render() {
+    const {height, width} = this.props;
+    return <canvas height={height} width={width}></canvas>;
+  }
+}
+
 class WaveformChannel extends React.Component {
+  constructor() {
+    super(...arguments);
+    this.state = {
+      height: null,
+      width: null
+    };
+    this.onResize = this._onResize.bind(this);
+  }
+
+  _onResize() {
+    this.updateDimensions();
+  }
+
+  componentDidMount() {
+    this.updateDimensions();
+    window.addEventListener('resize', this.onResize);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.onResize);
+  }
+
+  updateDimensions() {
+    this.setState({
+      height: this.container.offsetHeight,
+      width: this.container.offsetWidth
+    });
+  }
+
   render() {
     const {channel} = this.props;
+    const {height, width} = this.state;
     const className = channel === 'left' ? styles.leftChannel : styles.rightChannel;
+
     return (
-      <div className={className}>Channel</div>
+      <div className={className} ref={container => {this.container = container;}}>
+        {height && width ? <WaveformCanvas height={height} width={width} /> : null}
+      </div>
     );
   }
 }
